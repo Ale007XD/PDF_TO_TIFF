1b72c70cf9d1329274c55fbb1ca5b0b4a91fae52
Ale007XD <alex.deloverov@gmail.com>
2025-08-17 13:21:29 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,5 +5,10 @@ def is_safe_filename(fn):
     return not any(x in fn for x in forbidden) and fn.strip() != ""
 
 def get_file_size_mb(file):
-    # aiogram Document
-    return file.file_size / 1024 / 1024
+    # aiogram Document или путь до файла
+    if hasattr(file, "file_size"):
+        return file.file_size / 1024 / 1024
+    elif isinstance(file, str) and os.path.isfile(file):
+        return os.path.getsize(file) / 1024 / 1024
+    else:
+        raise ValueError("Некорректный тип для вычисления размера файла")
